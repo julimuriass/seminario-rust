a4ec0ee43a6a95763fa679a549b5a1366aa02d10
julimuriass <julieta.murias@gmail.com>
2025-04-19 15:52:24 -0300

    Started exercise 5. Unfinished.

---
+++ src/tp02/ej05.rs
@@ -0,0 +1,16 @@
+pub fn act05(){
+
+    fn doble_numeros(numbers:[f64 ; 10]) -> [f64 ; 10]{
+        let mut arreglo_final:[f64 ; 10];
+
+        for number in numbers.iter(){
+            
+        }
+    }
+
+
+    let numeros:[f64 ; 10] = [0.0 , 1.1 , 2.2 , 3.3 , 4.4 , 5.5 , 6.6 , 7.7 , 8.8 , 9.9];
+    let doble_numeros:[f64 ; 10] = duplicar_valores(numeros);
+
+
+}
