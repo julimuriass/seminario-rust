285633bd0655e0f9899f740e187ad12231c70840
julimuriass <julieta.murias@gmail.com>
2025-04-21 15:47:17 -0300

    Added exercise 6 (unchecked). Have probs with rust

---
+++ src/tp02/ej06.rs
@@ -0,0 +1,24 @@
+//# [should_panic!]
+# [test]
+
+pub fn act06(){
+
+    fn longitud_de_cadenas(strings:[String ; 3]) -> [u32 ; 3]{
+
+        let mut longitudes:[u32 ; 3];
+        for n in 0..strings.len(){
+            longitudes[n] = strings[n].len();
+        }
+
+        longitudes
+
+    }
+      
+
+
+    let arreglo_strings:[String ; 3] = [String::from("Hello") , String::from("world") , String::from("!")]; //preg if okay
+    let output:[u32 ; 3] = longitud_de_cadenas (arreglo_strings);
+
+    assert_eq!(output[0] , 5);
+
+}
