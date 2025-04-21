db2f5ac3d50bb8b4d7f65df0bb1b70267f2d26d4
julimuriass <julieta.murias@gmail.com>
2025-04-21 16:00:00 -0300

    Fixed ex6, and the prob with rust.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     //tp02::ej02::act2();
     //tp02::ej03::act03();
     //tp02::ej04::act04();
-    tp02::ej05::act05();
-    //tp02:ej06::act06();
+    //tp02::ej05::act05();
+    tp02::ej06::act06();
 
 }
--- src/tp02/ej06.rs
+++ src/tp02/ej06.rs
@@ -1,13 +1,13 @@
-//# [should_panic!]
+# [should_panic]
 # [test]
 
 pub fn act06(){
 
     fn longitud_de_cadenas(strings:[String ; 3]) -> [u32 ; 3]{
 
-        let mut longitudes:[u32 ; 3];
+        let mut longitudes:[u32 ; 3] = [0 , 0 , 0];
         for n in 0..strings.len(){
-            longitudes[n] = strings[n].len();
+            longitudes[n] = strings[n].len() as u32;
         }
 
         longitudes
@@ -19,6 +19,6 @@ pub fn act06(){
     let arreglo_strings:[String ; 3] = [String::from("Hello") , String::from("world") , String::from("!")]; //preg if okay
     let output:[u32 ; 3] = longitud_de_cadenas (arreglo_strings);
 
-    assert_eq!(output[0] , 5);
+    assert_eq!(output[0] , 8);
 
 }
