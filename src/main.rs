ef28fd2743c4f8a1b2dce4fdb7b7614d8ca05072
julimuriass <julieta.murias@gmail.com>
2025-05-02 10:31:08 -0300

    Deleted tp02 in Main. Added tp03. Deleted an extra file.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,5 @@
 mod tp02;
 
 
-fn main() { //preg si así está okay al final?????? 
-
-    //tp02::ej01::es_par(2); 
-
-    //tp02::ej02::es_primo(7);
-
-    //let numeros:[i32; 10] = [1,2,3,4,5,6,7,8,9,10];
-    //tp02::ej03::suma_pares(numeros);
-
-    //let numeros:[i32 ; 10] = [1,2,3,4,5,6,7,8,9,10];
-    //tp02::ej04::act04(numeros);
-
-    //let numeros:[f64 ; 5] = [0.0 , 1.1 , 2.2 , 3.3 , 4.4];
-    //tp02::ej05::duplicar_valores(numeros);
-
-    //let arreglo_strings:[String ; 3] = [String::from("Hello") , String::from("world") , String::from("!")];
-    //tp02::ej06::longitud_de_cadenas(arreglo_strings);
-
-    //let numeros:[i32 ; 10] = [-5 , 800 , -30, -2 , -1 , 0 , 244 , 9999 , 11 , 12];
-    //let limite = 0;
-    //tp02::ej07::cant_mayores(numeros , limite);
-
-    //let numbers1:[f64 ; 2]= [0.0 , 25.2];
-    //let numbers2:[f64 ; 2]= [-2.4 , 3.1];
-    //tp02::ej08::sumar_arreglos(numbers1 , numbers2);
-
-    //let numbers:[i32 ; 10]= [0,1,2,3,4,5,6,7,8,9];
-    //let inferior = 1;
-    //let superior = 5;
-    //tp02::ej09::cantidad_en_rango(numbers , inferior , superior);
-
-    //let strings:[String ; 4] = [String::from("Hello") , String::from("Abracadabra") , String::from("Eye") , String::from("Banana")];
-    //let limite:u32 = 4;
-    //tp02::ej10::cantidad_de_cadenas_mayor_a(strings , limite);
-
-    //let mut numbers:[i32 ; 5] = [1 , 2 , 3 , 4 , 5];
-    //let factor: i32 = -1;
-    //tp02::ej11::multiplicar_valores(numbers , factor);
-
-    //let mut numbers:[i32 ; 10] = [1,2,3,4,5,6,7,8,9,10];
-    //tp02::ej12::reemplazar_pares(numbers);
-
-    //let mut names:[String ; 7] = [String::from("Juli") , String::from("Benja") , String::from("Gonza") , String::from("Fio") , String::from("Angi") , String::from("Emi") , String::from("Angel")];
-    //tp02::ej13::ordenar_nombres(names);
-
-    //let mut number:f64 = 0.0;
-    //tp02::ej14::incrementar(number);
+fn main() { 
 }
