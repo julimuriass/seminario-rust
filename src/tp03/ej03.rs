5802f4081d10e07612c617787a4158f1f7634b31
julimuriass <julieta.murias@gmail.com>
2025-05-04 17:26:36 -0300

    Exercise 3, tp03.

--- src/tp03/ej03.rs
+++ src/tp03/ej03.rs
@@ -5,7 +5,6 @@ struct Date {
 }
 
 impl Date {
-
     fn new (day: u32, month: u32, year: u32) -> Date {
         Date {
             day,
@@ -37,7 +36,86 @@ impl Date {
         self.day >= 1 && self.day <= max_days
     }
 
-    fn sum_days (&self, days: u32) {
-        
+    fn sum_days (&mut self, mut days: u32) {
+        let days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let mut days_in_current_month = days_in_month[self.month as usize];
+
+        if self.month = 2 && self.is_leap_year() {
+            days_in_current_month = 29;
+        }
+
+        //Calculate the remaining days in the current month.
+        let mut remaining_days_in_month = days_in_current_month - self.day;
+
+        while days > remaining_days_in_month {
+            days -= remaining_days_in_month + 1; //+1 ensures that the current day is included when transitioning to the next month.
+
+            self.day = 1; //Move to the next month.
+            self.month += 1;
+
+            if (self.month > 12) {
+                self.year += 1;
+                self.month = 1;
+            }
+
+            //Recalculate the days in the current month.
+            days_in_current_month = days_in_month[self.month as usize];
+            if self.month = 2 && self.is_leap_year() {
+                days_in_current_month = 29;
+            } 
+
+            remaining_days_in_month = days_in_current_month - self.day;
+        }
+        self.day += days;  
+    }
+
+    fn subtract_days (&mut self, mut days: u32) {
+        let days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let mut days_in_current_month = days_in_month[self.month as usize];
+
+        if self.month = 2 && self.is_leap_year() {
+            days_in_current_month = 29;
+        }
+
+        while days > self.days { //While the numbers of days I have to subtract is greater than the days I already have.
+            days -= self.days + 1; //+1 ensures that the current day is included when transitioning to the previous month.
+
+            self.month -= 1;
+            if (self.month < 1) {
+                self.year -= 1;
+                self.month = 12;
+            }   
+
+            //Recalculate the days in the current month.
+            days_in_current_month = days_in_month[self.month as usize];
+            if self.month = 2 && self.is_leap_year() {
+                days_in_current_month = 29;
+            } 
+
+            self.day = days_in_current_month; //Move to the end of the previous month.
+
+        }
+
+        self.day -= days;  
+    }
+
+    fn is_greater (&self, other: &Date) -> bool {
+
+        //First check the year.
+        if other.year > self.year {
+            true
+        } else {
+            false
+        }
+
+        //Check month.
+        if other.month > self.month {
+            true
+        } else {
+            false
+        }
+
+        //If months are equal, compare day.
+        other.day > self.day
     }
 }
