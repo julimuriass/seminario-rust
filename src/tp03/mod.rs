2271c4133fd134471df9df2aa559deaac29c2bf4
julimuriass <julieta.murias@gmail.com>
2025-05-16 11:55:38 -0300

    Renamed ex06, tp03 to julieta_murias_v2.rs.

--- src/tp03/mod.rs
+++ src/tp03/mod.rs
@@ -3,10 +3,10 @@ pub mod ej02;
 pub mod ej03;
 pub mod ej04;
 pub mod ej05;
-pub mod ej06;
+pub mod julieta_murias_v1;
 pub mod ej07;
 pub mod ej08;
 pub mod ej09;
 pub mod ej10;
 
-pub mod ej06_2;
+pub mod julieta_murias_v2;
