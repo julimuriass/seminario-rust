98ef76ce43905d0c1593709e7e2ccdd1c62d845b
julimuriass <julieta.murias@gmail.com>
2025-05-26 00:03:30 -0300

    Started again ex03, tp04. Created fn upgrade and fn downgrade.

--- src/tp04/ej03.rs
+++ src/tp04/ej03.rs
@@ -1,7 +1,9 @@
 use crate::tp03::ej03::Fecha;
 
-struct StreamingRust {
-    usuarios: Vec<Usuario>,
+enum TipoSuscripcion {
+    Basic,
+    Clasic,
+    Super,
 }
 
 struct Suscripcion {
@@ -12,89 +14,89 @@ struct Suscripcion {
     activa: bool,
 }
 
-struct Usuario {
-    nombre: String,
-    email: String,
-    username: String,
-    suscripciones: Vec<Suscripcion>,
-    medio_pago: MedioPago,
-}
-
-enum TipoSuscripcion {
-    Basic,
-    Clasic, 
-    Super,
-}
-
-enum MedioPago {
-    Efectivo,
-    MercadoPago,
-    TransferenciaBancaria,
-    Cripto,
-    TarjetaCredito,
-}
-
-impl Usuario {
-    fn crear_usuario (nombre: String, email: String, username: String, medio_pago: MedioPago, suscripcion: Suscripcion) -> Usuario {
-        Usuario {
-            nombre,
-            email,
-            username,
-            suscripciones : vec![suscripcion],
-            medio_pago,
+impl Suscripcion {
+    fn downgrade (&mut self) {
+        match self.tipo {
+            TipoSuscripcion::Basic => {self.activa = false;}
+            TipoSuscripcion::Clasic => {self.tipo = TipoSuscripcion::Basic;}
+            TipoSuscripcion::Super => {self.tipo = TipoSuscripcion::Clasic;}
         }
-    } 
+    }
 
-    fn suscripcion_activa (&self) -> Option<&Suscripcion> {
-        self.suscripciones.iter().find(|s| s.activa == true)
+    fn upgrade (&mut self) {
+        match self.tipo {
+            TipoSuscripcion::Basic => {self.tipo = TipoSuscripcion::Clasic;}
+            TipoSuscripcion::Clasic => {self.tipo = TipoSuscripcion::Super;}
+            _ => {}
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_downgrade() {
+        let mut suscripcion = Suscripcion{tipo: TipoSuscripcion::Basic, 
+                                                    costo_mensual: 100.0, duracion_meses: 1,
+                                                    fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
+                                                    activa: true};
         
+        assert_eq!(suscripcion.activa, true);
+        suscripcion.downgrade();
+        assert_eq!(suscripcion.activa, false); //From basic to cancelled. Ok.
+
+        //From super to clasic.
+        let mut suscripcion1 = Suscripcion{tipo: TipoSuscripcion::Super, 
+            costo_mensual: 100.0, duracion_meses: 1,
+            fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
+            activa: true};
+        
+        let is_super = match suscripcion1.tipo {
+            TipoSuscripcion::Super => true,
+            _ => false,
+        };
+
+        assert!(is_super);
+
+        suscripcion1.downgrade();
+        assert_eq!(suscripcion1.activa, true);
+        
+        let is_clasic = match suscripcion1.tipo {
+            TipoSuscripcion::Clasic => true,
+            _ => false,
+        };
+        assert_eq!(is_clasic, true);
+    }
+
+    #[test]
+    fn test_upgrade() {
+        //From basic to clasic.
+        let mut suscripcion = Suscripcion{tipo: TipoSuscripcion::Basic, 
+            costo_mensual: 100.0, duracion_meses: 1,
+            fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
+            activa: true};
+        
+        suscripcion.upgrade();
+        let is_clasic = match suscripcion.tipo {
+            TipoSuscripcion::Clasic => true,
+            _ => false,
+        };
+        assert_eq!(is_clasic, true);
+
+        //From super to super (nothing changes).
+        let mut suscripcion1 = Suscripcion{tipo: TipoSuscripcion::Super, 
+            costo_mensual: 100.0, duracion_meses: 1,
+            fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
+            activa: true};
+        
+        suscripcion1.upgrade();
+        let is_super = match suscripcion1.tipo {
+            TipoSuscripcion::Super => true,
+            _ => false,
+        };
+        assert_eq!(is_super, true);
+
     }
 }
-
-impl StreamingRust {
-    fn upgrade_suscripcion (&mut self, usuario: &Usuario) {
-        //Find the user.
-        if let Some(user) = self.usuarios.iter_mut().find(|u| compare_users(&u, &usuario)) {
-            //Find the subscription I want to upgrade.
-            if let Some(){
-                match subscription.tipo {
-                    TipoSuscripcion::Basic => {subscription.tipo = TipoSuscripcion::Clasic;}
-                    TipoSuscripcion::Clasic => {subscription.tipo = TipoSuscripcion::Super;}
-                    _ => {}
-                }
-            } 
-        }
-    }
-
-    fn downgrade_suscripcion (&mut self, usuario: &Usuario) {
-        //Find the user.
-        if let Some(user) = self.usuarios.iter_mut().find(|u| compare_users(&u, &usuario)) {
-            //Find the subscription I want to upgrade.
-            if let Some(subscription) = user.suscripciones.iter_mut().find(|s| s.activa == true) {
-                match subscription.tipo {
-                    TipoSuscripcion::Basic => {subscription.tipo = TipoSuscripcion::Clasic;}
-                    TipoSuscripcion::Clasic => {subscription.tipo = TipoSuscripcion::Super;}
-                    _ => {}
-                }
-            } 
-        }
-    }
-}
-
-pub fn compare_users (user1: &Usuario, user2: &Usuario) -> bool {
-    user1.email.eq_ignore_ascii_case(&user2.email) &&
-    user1.nombre.eq_ignore_ascii_case(&user2.nombre) &&
-    comparar_medio_pagos (&user1.medio_pago, &user2.medio_pago)
-}
-
-pub fn comparar_medio_pagos (medio1: &MedioPago, medio2: &MedioPago) -> bool {
-    match (medio1, medio2) {
-        (MedioPago::Cripto, MedioPago::Cripto) => true,
-        (MedioPago::Efectivo, MedioPago::Efectivo) => true,
-        (MedioPago::MercadoPago, MedioPago::MercadoPago) => true,
-        (MedioPago::TarjetaCredito, MedioPago::TarjetaCredito) => true,
-        (MedioPago::TransferenciaBancaria, MedioPago::TransferenciaBancaria) => true,
-        (_) => false,
-    }
-}
-
