a17ed3e3678de92e59d04a124520a4b5140fcd32
julimuriass <julieta.murias@gmail.com>
2025-05-26 15:52:22 -0300

    TipoSuscripcion knows to upgrade and downgrade itself, how much it costs and what kind it is. Suscripción knows how to upgrade and downgrade a subscription.

--- src/tp04/ej03.rs
+++ src/tp04/ej03.rs
@@ -6,35 +6,105 @@ enum TipoSuscripcion {
     Super,
 }
 
+impl TipoSuscripcion {
+    fn costo_mensual(&self) -> f64 {
+        match self {
+            TipoSuscripcion::Basic => 5.0,
+            TipoSuscripcion::Clasic => 9.5,
+            TipoSuscripcion::Super => 15.0,
+        }
+    }
+
+    fn upgrade(&self) -> Option<TipoSuscripcion>{
+        match self {
+            TipoSuscripcion::Basic => Some(TipoSuscripcion::Clasic),
+            TipoSuscripcion::Clasic => Some(TipoSuscripcion::Super),
+            TipoSuscripcion::Super => None,
+        }
+    }
+
+    fn downgrade(&self) -> Option<TipoSuscripcion> {
+        match  self {
+            TipoSuscripcion::Basic => None,
+            TipoSuscripcion::Clasic => Some(TipoSuscripcion::Basic),
+            TipoSuscripcion::Super => Some(TipoSuscripcion::Clasic),
+            
+        }
+    }
+
+    //Tendría que implementar un trait para los soy_...() ???!!!
+    fn soy_basic(&self) -> bool {
+        match self {
+            TipoSuscripcion::Basic => true,
+            _ => false, 
+        }
+    }
+
+    fn soy_clasic(&self) -> bool {
+        match self {
+            TipoSuscripcion::Clasic => true,
+            _ => false, 
+        }
+    }
+
+    fn soy_super(&self) -> bool {
+        match self {
+            TipoSuscripcion::Super => true,
+            _ => false, 
+        }
+    }
+}
+
 struct Suscripcion {
     tipo: TipoSuscripcion,
-    costo_mensual: f64,
     duracion_meses: u8,
     fecha_inicio: Fecha,
     activa: bool,
 }
 
 impl Suscripcion {
-    fn downgrade (&mut self) {
-        match self.tipo {
-            TipoSuscripcion::Basic => {self.activa = false;}
-            TipoSuscripcion::Clasic => {self.tipo = TipoSuscripcion::Basic;}
-            TipoSuscripcion::Super => {self.tipo = TipoSuscripcion::Clasic;}
+    fn new(tipo: TipoSuscripcion, duracion_meses: u8, fecha_inicio: Fecha) -> Suscripcion {
+        Suscripcion {
+            tipo,
+            duracion_meses,
+            fecha_inicio,
+            activa: true,
         }
     }
 
-    fn upgrade (&mut self) {
-        match self.tipo {
-            TipoSuscripcion::Basic => {self.tipo = TipoSuscripcion::Clasic;}
-            TipoSuscripcion::Clasic => {self.tipo = TipoSuscripcion::Super;}
-            _ => {}
-        }
+    fn activar_suscripcion(&mut self) {
+        self.activa = true
     }
 
-    fn cancelarse (&mut self) {
+    fn desactivar_suscripcion(&mut self) {
         self.activa = false
     }
+
+    fn upgrade(&mut self) -> Result<(), String> {
+        match self.tipo.upgrade() {
+            Some(tipo_nuevo) => {
+                self.tipo = tipo_nuevo;
+                Ok(())
+            }
+            None => Err("No se puede ascender de Super.".to_string())  
+        }
+    }
+
+    fn downgrade(&mut self) -> Result<(), String> {
+        match self.tipo.downgrade() {
+            Some(tipo_nuevo) => {
+                self.tipo = tipo_nuevo;
+                Ok(())
+            }
+            None => {
+                self.desactivar_suscripcion();
+                Err("Se ha canclado su suscripción.".to_string())
+            }
+        }
+    }
 }
+
+
 
 
 #[cfg(test)]
@@ -42,79 +112,25 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_downgrade() {
-        let mut suscripcion = Suscripcion{tipo: TipoSuscripcion::Basic, 
-                                                    costo_mensual: 100.0, duracion_meses: 1,
-                                                    fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
-                                                    activa: true};
-        
-        assert_eq!(suscripcion.activa, true);
-        suscripcion.downgrade();
-        assert_eq!(suscripcion.activa, false); //From basic to cancelled. Ok.
+    fn test_upgrade_downgrade() {
+        let mut suscripcion0 = Suscripcion::new(TipoSuscripcion::Basic, 3, Fecha { dia: (12), mes: (4), año: (2020) });
+        let mut suscripcion1 = Suscripcion::new(TipoSuscripcion::Super, 3, Fecha { dia: (12), mes: (4), año: (2020) });
 
-        //From super to clasic.
-        let mut suscripcion1 = Suscripcion{tipo: TipoSuscripcion::Super, 
-            costo_mensual: 100.0, duracion_meses: 1,
-            fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
-            activa: true};
-        
-        let is_super = match suscripcion1.tipo {
-            TipoSuscripcion::Super => true,
-            _ => false,
-        };
+        //Test upgrade. Ok.
+        assert_eq!(suscripcion0.tipo.soy_basic(), true);
+        assert!(suscripcion0.upgrade().is_ok());
+        assert_eq!(suscripcion0.tipo.soy_clasic(), true);
 
-        assert!(is_super);
+        assert!(suscripcion1.upgrade().is_err());
 
-        suscripcion1.downgrade();
-        assert_eq!(suscripcion1.activa, true);
-        
-        let is_clasic = match suscripcion1.tipo {
-            TipoSuscripcion::Clasic => true,
-            _ => false,
-        };
-        assert_eq!(is_clasic, true);
+        //Test downgrade. Ok.
+        assert!(suscripcion0.downgrade().is_ok());
+        assert_eq!(suscripcion0.tipo.soy_basic(), true);
+        assert!(suscripcion0.downgrade().is_err());
+        assert_eq!(suscripcion0.activa, false);
     }
 
-    #[test]
-    fn test_upgrade() {
-        //From basic to clasic.
-        let mut suscripcion = Suscripcion{tipo: TipoSuscripcion::Basic, 
-            costo_mensual: 100.0, duracion_meses: 1,
-            fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
-            activa: true};
-        
-        suscripcion.upgrade();
-        let is_clasic = match suscripcion.tipo {
-            TipoSuscripcion::Clasic => true,
-            _ => false,
-        };
-        assert_eq!(is_clasic, true);
-
-        //From super to super (nothing changes).
-        let mut suscripcion1 = Suscripcion{tipo: TipoSuscripcion::Super, 
-            costo_mensual: 100.0, duracion_meses: 1,
-            fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
-            activa: true};
-        
-        suscripcion1.upgrade();
-        let is_super = match suscripcion1.tipo {
-            TipoSuscripcion::Super => true,
-            _ => false,
-        };
-        assert_eq!(is_super, true);
-
-    }
-
-    #[test]
-    fn test_cancelarse() {
-        let mut suscripcion = Suscripcion{tipo: TipoSuscripcion::Basic, 
-            costo_mensual: 100.0, duracion_meses: 1,
-            fecha_inicio: Fecha {dia: (12), mes: (3), año: (2000)},
-            activa: true};
-
-        assert!(suscripcion.activa);
-
-        suscripcion.cancelarse();
-        assert_eq!(suscripcion.activa, false); //Test Ok. A suscription knows how to cancel itself.
-    }
+    
+ 
+    
 }
